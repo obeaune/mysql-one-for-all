d8cc7325870e599dac116e619c3f1c4b6c0f4538
obeaune <linecora86@gmail.com>
2022-05-10 12:44:26 -0300

    Desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,10 @@
+SELECT user_name AS usuario,
+	COUNT(*) AS qtde_musicas_ouvidas,
+	ROUND((SUM(SG.song_duration_seconds) / 60), 2) AS total_minutos
+FROM SpotifyClone.user AS US
+INNER JOIN SpotifyClone.reproduction_history AS REP
+ON US.user_id = REP.user_id
+INNER JOIN SpotifyClone.song AS SG
+ON REP.song_id = SG.song_id
+GROUP BY US.user_id
+ORDER BY usuario;
